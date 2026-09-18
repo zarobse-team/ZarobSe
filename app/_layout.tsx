d69cdd3b2926ef9bc2d28ab2f51eac246372b88f
opoczan <jakubopoka@MacBook-Pro-opoczan.local>
2026-09-18 15:19:22 +0200

    aktualizacja do SDK 57 i api

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,31 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from "expo-router/react-navigation";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: "(tabs)",
+	anchor: "(tabs)",
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+			<Stack screenOptions={{ headerShown: false }}>
+				<Stack.Screen name='(tabs)' />
+				<Stack.Screen
+					name='modal'
+					options={{ presentation: "modal", title: "Modal" }}
+				/>
+			</Stack>
+			<StatusBar style='auto' />
+		</ThemeProvider>
+	);
 }
--- constants/api.ts
+++ constants/api.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL =
+	process.env.EXPO_PUBLIC_API_URL ?? "https://zarobse.onrender.com";
